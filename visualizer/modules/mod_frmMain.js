9501a8b548ef56134d50f32bb2f869b4e15e893c
Guilherme Ventura <guilhermeventura2@gmail.com>
2015-08-31 17:40:07 -0300

    Add 'Comics' button, to create the comics form

--- visualizer/modules/mod_frmMain.js
+++ visualizer/modules/mod_frmMain.js
@@ -1,12 +1,18 @@
 function mod_frmMain() {
   function init() {
     frmMain.flxcRoot.btnOpenCharactersForm.onClick = btnOpenCharactersForm_onClick;
+    frmMain.flxcRoot.btnOpenComicsForm.onClick = btnOpenComicsForm_onClick;
   }
   
   /* EVENT HANDLERS */
   function btnOpenCharactersForm_onClick() {
     // TODO: open frmCharactersList form
     alert('***Open frmCharactersList form***');
+  }
+  
+  function btnOpenComicsForm_onClick() {
+    // TODO: open frmCharactersList form
+    alert('***Open frmComicsList form***');
   }
   
   init();
