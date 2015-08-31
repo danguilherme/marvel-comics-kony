dc02c2f373732262ca7bcb9acdaf993b352429f5
Guilherme Ventura <guilhermeventura2@gmail.com>
2015-08-31 17:33:29 -0300

    Add main form, with init action mapped to its module function; Uploaded to Cloud for App Preview (id B4CLW)

---
+++ visualizer/actions/mobile/ASfrmMain.js
@@ -0,0 +1,7 @@
+function ASfrmMain() {
+return AS_Form_23c49759eabc4f8ab52dd3f351123ccb();
+}
+function AS_Form_23c49759eabc4f8ab52dd3f351123ccb() {
+mod_frmMain.call(this);
+
+}
---
+++ visualizer/modules/mod_frmMain.js
@@ -0,0 +1,13 @@
+function mod_frmMain() {
+  function init() {
+    frmMain.flxcRoot.btnOpenCharactersForm.onClick = btnOpenCharactersForm_onClick;
+  }
+  
+  /* EVENT HANDLERS */
+  function btnOpenCharactersForm_onClick() {
+    // TODO: open frmCharactersList form
+    alert('***Open frmCharactersList form***');
+  }
+  
+  init();
+}
