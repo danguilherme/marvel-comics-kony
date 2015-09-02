03faee08fde452aba8c22ba01cc7fed1b1943af9
Guilherme Ventura <guilhermeventura2@gmail.com>
2015-09-02 14:29:59 -0300

    Improve components separation on Characters form

--- visualizer/modules/form_frmCharacters.js
+++ visualizer/modules/form_frmCharacters.js
@@ -1,8 +1,12 @@
 function form_frmCharacters() {
   function init() {
+    frmCharacters.flxcRoot.segCharacters.onRowClick = segCharacters_onRowClick;
   }
   
   /* EVENT HANDLERS */
+  function segCharacters_onRowClick() {
+    alert(arguments);
+  }
   
   init();
 }
